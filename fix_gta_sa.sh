a9f9bfb6dcab970e2f6be0d9e5f1dfae6e91ff33
rogue <rogue87.dev@gmail.com>
2024-03-14 22:29:52 +0200

    If you think you know bash scripting, then you don't know bash scripting.

--- fix_gta_sa.sh
+++ fix_gta_sa.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
-set -euo
+# set -euo
 
+### Patches
 # # First, download SilentPatch
 # wget --no-check-certificate https://silent.rockstarvision.com/uploads/SilentPatchSA.zip -O SilentPatch.zip
 
-# Just for reference
-# release_url=$(curl -s https://api.github.com/repos/$owner/$repo/releases/latest)
 # Download mods and patches from github
 owners=(ThirteenAG thelink2012 ThirteenAG)
 repos=(Ultimate-ASI-Loader modloader III.VC.SA.WindowedMode)
-filenames=("Ultimate-ASI-Loader.zip" "modloader.zip" "III.VC.SA.WindowedMode.zip")
+# filenames=("Ultimate-ASI-Loader.zip" "modloader.zip" "III.VC.SA.WindowedMode.zip")
 
-if [[ condition ]]; then
-  
+if [[ ${#owners[@]} == ${#repos[@]} ]]; then
+  for ((i = 0; i < ${#repos[@]}; i++)); do
+    # Capture download URL
+    url=$(curl -s https://api.github.com/repos/${owners[$i]}/${repos[$i]}/releases/latest | jq '.assets[0].browser_download_url')
+
+    if [[ ! -z "$url" ]]; then
+      echo "Downloading ${repos[i]}"
+      wget $(echo "$url" | sed 's/"//g')
+    else
+      echo "Error: Failed to download ${repo[i]}"
+    fi
+  done
 fi
-# for ((i = 0; i < ${#repos[@]}; i++)); do
-#   echo https://api.github.com/repos/${owners[$i]}/${repos[$i]}/releases/latest
-# done
+
+### Mods
