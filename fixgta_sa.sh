234a4b2cbdbfa7c28a582f5736dfc075355ae6d3
rogue <rogue87.dev@gmail.com>
2024-03-22 08:33:17 +0200

    update script

--- fixgta_sa.sh
+++ fixgta_sa.sh
@@ -40,68 +40,72 @@ downloadGtaGarage(){
   echo
 }
 
-## Silent's patches/mods
-downloadRSV https://silent.rockstarvision.com/uploads/SilentPatchSA.zip "SilentPatchSA"
-downloadRSV https://silent.rockstarvision.com/uploads/GInputSA.zip "Ginput"
+installLatest(){
+  ## Silent's patches/mods
+  downloadRSV https://silent.rockstarvision.com/uploads/SilentPatchSA.zip "SilentPatchSA"
+  downloadRSV https://silent.rockstarvision.com/uploads/GInputSA.zip "Ginput"
 
-## GtaGrage
-downloadGtaGarage https://www.gtagarage.com/mods/download.php?f=35121 "wshps.rar" "Widescreen HOR+ Support" 
+  ## GtaGrage
+  downloadGtaGarage https://www.gtagarage.com/mods/download.php?f=35121 "wshps.rar" "Widescreen HOR+ Support" 
 
-## Download mods and patches from github
-github_repos=(
-  ThirteenAG/Ultimate-ASI-Loader
-  thelink2012/modloader
-  cleolibrary/CLEO4
-  ThirteenAG/III.VC.SA.WindowedMode
-  aap/skygfx
+  ## Download mods and patches from github
+  github_repos=(
+    ThirteenAG/Ultimate-ASI-Loader
+    thelink2012/modloader
+    cleolibrary/CLEO4
+    ThirteenAG/III.VC.SA.WindowedMode
+    aap/skygfx
+  )
+  downloadGithub "${github_repos[@]}"
+
+  ## Download mods and patches from github repos with tags
+  # Project2DFX by ThirteenAG
+  url=$(wget -qO- https://api.github.com/repos/ThirteenAG/III.VC.SA.IV.Project2DFX/releases/1159120 | jq '.assets[0].browser_download_url')
+  echo "Downloading Project2DFX"
+  wget -q --show-progress $(echo "$url" | sed 's/"//g')
+  echo
+
+  # GTASA.WidescreenFix by ThirteenAG
+  echo "Downloading WidescreenFix"
+  wget -q --show-progress https://github.com/ThirteenAG/WidescreenFixesPack/releases/download/gtasa/GTASA.WidescreenFix.zip
+  echo
+
+  ## Download mods/patches from my repo
+  #
+}
+### End
+
+installStable(){
+  echo "Stil Work in progress..."
+  sleep 1s
+}
+
+echo "Fix GTA-SA"
+echo "Install ..."
+opts=(
+  "Latest mods/patches (Better compatibility with newer mods)"
+  "Stable mods/patches (Worse compatibility with newer mods but better stability)" 
+  "Exit"
 )
-downloadGithub "${github_repos[@]}"
-
-## Download mods and patches from github repos with tags
-# Project2DFX by ThirteenAG
-url=$(wget -qO- https://api.github.com/repos/ThirteenAG/III.VC.SA.IV.Project2DFX/releases/1159120 | jq '.assets[0].browser_download_url')
-echo "Downloading Project2DFX"
-wget -q --show-progress $(echo "$url" | sed 's/"//g')
-echo
-
-# GTASA.WidescreenFix by ThirteenAG
-echo "Downloading WidescreenFix"
-wget -q --show-progress https://github.com/ThirteenAG/WidescreenFixesPack/releases/download/gtasa/GTASA.WidescreenFix.zip
-echo
-
-## Download mods/patches from my repo
-#
-
-### End
-
-### Download Stable Mods & Patches 
-### End
-
-# echo "Fix GTA-SA"
-# echo "Install ..."
-# opts=(
-#   "Latest mods/patches (Better compatibility with newer mods)"
-#   "Stable mods/patches (Worse compatibility with newer mods but better stability)" 
-#   "Exit"
-# )
-# PS3="Please select an option: "  # Set the prompt for user input
-# select opt in "${opts[@]}"; do
-#   # Code to execute based on the chosen option
-#   case $REPLY in
-#     1)
-#       echo "You selected Option 1"
-#       ;;
-#     2)
-#       echo "You selected Option 2"
-#       ;;
-#     3)
-#       echo "Quitting..."
-#       sleep 2s
-#       echo "Goodbye :D"
-#       ;;
-#     *) echo
-#       "Invalid option"
-#       ;;
-#   esac
-#   break  # Exit the loop after a selection is made
-# done
+PS3="Please select an option: "  # Set the prompt for user input
+select opt in "${opts[@]}"; do
+  # Code to execute based on the chosen option
+  case $REPLY in
+    1)
+      echo "Installing latest mods and patches"
+      downloadLatest
+      ;;
+    2)
+      echo "You selected Option 2"
+      ;;
+    3)
+      echo "Quitting..."
+      sleep 2s
+      echo "Goodbye :D"
+      ;;
+    *) echo
+      "Invalid option"
+      ;;
+  esac
+  break  # Exit the loop after a selection is made
+done
